543dd91e41578e1f32f2b3b61547721868bf4b62
helloteacher-alt <panyisa20012544@gmail.com>
2022-10-04 20:26:14 +0700

    no message

--- Assets/script/Player/PlayerControls.cs
+++ Assets/script/Player/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/script/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/script/Player/PlayerControls.inputactions'
 
 using System;
 using System.Collections;

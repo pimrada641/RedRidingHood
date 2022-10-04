249487cfa81c406b9483d529ab8aac9577bc6eca
helloteacher-alt <panyisa20012544@gmail.com>
2022-10-04 21:57:28 +0700

    Clean functions to 'Health.cs'

--- Assets/script/Health/HealthBar.cs
+++ Assets/script/Health/HealthBar.cs
@@ -8,27 +8,40 @@ public class HealthBar : MonoBehaviour
     public GameObject gameoverscreen;
     private Image health_Bar;
     public float currentHealth;
-    private float MaxHealth = 100f;
+    public float MaxHealth = 100f;
     void Start()
     {
         health_Bar = GetComponent<Image>();
-        
     }
 
     void Update()
     {
-        if(this.gameObject.CompareTag("Enemy")){
-            currentHealth = EnemyScript.currentHP;
-            health_Bar.fillAmount = currentHealth/MaxHealth;
+        if(this.gameObject.CompareTag("Enemy"))
+        {
+            BloodCount(EnemyScript.currentHP);
         }
-        if(this.gameObject.CompareTag("Player")){
-            currentHealth = ChraracterScript.HP;
-            health_Bar.fillAmount = currentHealth/MaxHealth;
-            if(currentHealth == 0){
-                gameoverscreen.SetActive(true);
-                health_Bar.fillAmount = 0;
-                Time.timeScale = 0f;
+        if(this.gameObject.CompareTag("Player"))
+        {
+            BloodCount(ChraracterScript.HP);
+            if(currentHealth == 0)
+            {
+              ShowGameOver();
             }
         }
     }
+
+    void BloodCount(float currentHP)
+    {
+        currentHealth = currentHP;
+        health_Bar.fillAmount = currentHealth/MaxHealth;
+        return;
+    }
+
+    void ShowGameOver()
+    {
+        gameoverscreen.SetActive(true);
+        health_Bar.fillAmount = 0;
+        Time.timeScale = 0f;
+    }
 }
+

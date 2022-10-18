df7f250791df80d9e12d5e0635f6a88fb18d3db0
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2022-10-19 01:49:24 +0700

    Created Character Database Class

---
+++ Assets/script/Player/CarData.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Serializable]
+public class CarData{
+    public string name;
+    public string des;
+}

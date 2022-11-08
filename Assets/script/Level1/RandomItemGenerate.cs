9d742268e088ae238676fbefcfa81382a3beb505
pimrada 031 <61728666+pimrada641@users.noreply.github.com>
2022-11-08 14:51:29 +0700

    Use "Min()", "Any()", "All()"

--- Assets/script/Level1/RandomItemGenerate.cs
+++ Assets/script/Level1/RandomItemGenerate.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class RandomItemGenerate : MonoBehaviour
 {
@@ -14,20 +15,29 @@ public class RandomItemGenerate : MonoBehaviour
 
     IEnumerator randomItemgen(){
         yield return new WaitForSeconds(4);
+
         int randomNumber = Random.Range(1,20);
-        if(randomNumber == 1)
+
+        List<int> RanNum = new List<int>();
+        RanNum.Add(randomNumber);
+
+        var findMin = RanNum.Min();
+        var result1 = RanNum.Any(x => x==1);
+        var result2 = RanNum.All(x => x==2);
+
+        if(result1)
         {
             InstantiateItem(Prefab2);
         }
-        else if(randomNumber == 2)
+        else if(result2)
         {
             InstantiateItem(Prefab3);
         }
-        else
+        else if(findMin>2)
         {
-            Debug.Log("Gen3");
             InstantiateItem(Prefab1);
         }
+        RanNum.Clear();
     }
 
     void InstantiateItem(GameObject prefab)
